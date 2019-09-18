3366967fe442651985b5f8cf2b1db89e25d8e381
daeun <wjdekdms99@naver.com>
2019-09-18 16:32:56 +0900

    complete project configuration

---
+++ src/test/java/com/webapp/woriga/WorigaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webapp.woriga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorigaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
